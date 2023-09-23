7f0d674eccd43395dd4924a5c74b98b2c2d8d679
Nils Hansen <nils.hansen@akros.ch>
2023-09-23 23:24:16 +0200

    use angular tree component

--- knowledgebase/src/app/features/sidenav/component/sidenav.component.ts
+++ knowledgebase/src/app/features/sidenav/component/sidenav.component.ts
@@ -1,17 +1,23 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {Folder} from "../api/folder";
+import {ITreeOptions, TreeModule} from "@odymaui/angular-tree-component";
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TreeModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent {
 
   currentlySelectedItem: Folder;
+
+  //angular-tree docs: https://angular2-tree.readme.io/docs
+  treeOptions: ITreeOptions = {
+    childrenField: 'childNodes',
+  };
 
   @Input() navItems: Folder[];
 
