19a3c6c146d22df4e54d2f318e19242845d34996
Nils Hansen <nils.hansen@yahoo.de>
2026-01-28 21:25:06 +0100

    update environment.prod.ts

--- knowledgebase/src/environments/environment.prod.ts
+++ knowledgebase/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://my-knowledgebase.duckdns.org/api/',
-  OAUTH_CALLBACK_URL: 'https://my-knowledgebase.duckdns.org/api/auth/google',
+  baseUrl: 'https://my-knowledgebase.ch/api/',
+  OAUTH_CALLBACK_URL: 'https://my-knowledgebase.ch/api/auth/google',
 };
